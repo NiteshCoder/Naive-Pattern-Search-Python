e8cb06c532bdb88c5bb853f987a03c76a3fb81b7
Niteshkumar Prajapati <73467521+NiteshCoder@users.noreply.github.com>
2021-01-06 22:49:05 +0530

    Update naive_pattern_search.py

--- naive_pattern_search.py
+++ naive_pattern_search.py
@@ -1,54 +1,6 @@
 
 #Naive Pattern Searching Algorithm
 '''
-- Naive searching algorithm mainy used for pattern searching from the given data.
-- Easiest algorithm to understand and perform.
-
-How it works?
-- Compare elements and if its successfully compared then ,
-  both the data and search Increment by 1 to compare further element.
-  
-- If both the element is different then ,
-  pattern again initilise to 0th index but Data not.
-
-Let's Understand with Example.
-- Example: 
-	
-	Data    = A B C D B C A B C A B C A B C
-	pattern = B C
-	
-	-Step 1 : A B C D B C A B C A B C A B C
-	/compare:   B C
-	
-	-Step 2 : A B C D B C A B C A B C A B C
-	/compare:     B C  
-
-	-Step 3 : A B C D B C A B C A B C A B C
-	/compare:       B C
-
-	-Step 4 : A B C D B C A B C A B C A B C
-	/compare:         B C
-	
-	-Step 4 : A B C D B C A B C A B C A B C
-	/compare:           B C
-	
-	-Step 4 : A B C D B C A B C A B C A B C
-	/compare:             B C
-	
-	-Step 4 : A B C D B C A B C A B C A B C
-	/compare:               B C
-
-	Etc.
-THANK YOU.
-
-For Better understanding please visit :
-	https://www.youtube.com/watch?v=k7UpWkVvajM&t=604s
-
-'''
-
-
-
-'''
 ord() = Used to convert character to integer.
 chr() = Used to convert integer to character.
 
